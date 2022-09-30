1d15f0ac9354f69fa0b56f5d53b2300b9940714d
takashi miyasato <on.the.line.mt215@gmail.com>
2022-09-30 23:43:19 +0900

    indexリクエスト受けてコントローラーでアクション処理してブラウザに表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'ch2209s/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/ch2209s', to: 'ch2209#index'
 end
